6e32b93477560b8aede97874c76954186a4ac324
Sandumini <it20019136@my.sliit.lk>
2022-05-27 14:00:51 +0530

    create user signup and signin

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const ObjectId = require("mongodb").ObjectID;
+var crypto = require("crypto");
+const salt = "59509e61e54a2c849d1f46186899f674"; //crypto.randomBytes(16).toString('hex');
+
+getHashPassword = function (password) {
+  var hash = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return hash;
+};
 
 //insert new user
 router.route("/insert").post((req, res) => {
@@ -85,6 +94,43 @@ router.route("/delete/:id").delete((req, res) => {
     })
     .catch((err) => {
       console.error(err);
+      res.json({ message: "something went wrong!", status: false });
+    });
+});
+
+//user signup
+router.route("/signup").post((req, res) => {
+  req.body.password = getHashPassword(req.body.password);
+
+  User.insertMany(req.body)
+    .then((result) => {
+      console.log(result);
+      res.json({ message: "User created successfully!", status: true });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ message: "something went wrong!", status: false });
+    });
+});
+
+//user signin
+router.route("/signin").post((req, res) => {
+  User.findOne({ username: req.body.username })
+    .then((result) => {
+      if (result.password == getHashPassword(req.body.password)) {
+        console.log(result);
+        res.json({
+          id: data.userId,
+          role: data.userRole,
+          status: true,
+        });
+        return;
+      } else {
+        res.json({ message: "Invalid login information", status: false });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
       res.json({ message: "something went wrong!", status: false });
     });
 });
