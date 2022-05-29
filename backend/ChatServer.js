e827ed10e97994d1a3e6941b9bfa825d736dcb04
Dinul Liyanage <dinulliyanage@gmail.com>
2022-05-29 19:09:33 +0530

    completed chat without styling

--- backend/ChatServer.js
+++ backend/ChatServer.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {Server}=require('socket.io')
 const http=require('http')
 const cors=require('cors')
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./users')
 
 const PORT=process.env.PORT||5000
 
@@ -12,12 +13,13 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:1234"
+        origin:"http://localhost:1234",
+        methods:["GET","POST"]
     }
 })
 
 //app.use(cors())
-app.use(router)
+//app.use(router)
 
 
 //socket paramter is passed from the frontend
@@ -27,13 +29,25 @@ io.on('connection',(socket)=>{
 
     socket.on('join',({itNumber,roomId}, callback)=>{
         console.log(itNumber,roomId)
-        //const error=true;
+        const {error,user}=addUser({id:socket.id,itNumber,roomId})
 
-        /*if(error){
-            callback({error:'error'})
-        }*/
+        if(error) return callback(error)
 
+        socket.emit('message',{user:'admin', text:`${user.itNumber},Welcome to the room ${user.roomId}`})
+        socket.broadcast.to(user.roomId).emit('message',{user:'admin', text:`${user.itNumber}, has joined `})
 
+        socket.join(user.roomId)
+
+        callback()
+
+    })
+
+    //user generated message
+    socket.on('sendMessage',(message,callback)=>{
+        const user=getUser(socket.id)
+        io.to(user.roomId).emit('message',{user:user.itNumber, text:message})
+
+        callback();
     })
 
 
--- frontend/components/Chat.jsx
+++ frontend/components/Chat.jsx
@@ -1,6 +1,8 @@
 import React,{useState,useEffect} from 'react'
 import queryString from 'query-string'
 import io, { Socket } from 'socket.io-client'
+import ScrollToBottom from 'react-scroll-to-bottom'
+import Message from './Message'
 
 let socket;
 
@@ -8,6 +10,8 @@ const Chat=({location})=>{
 
     const [itNumber,setItNumber]=useState('')
     const [roomId,setRoomId]=useState('');
+    const [message,setMessage]=useState('');
+    const [messages,setMessages]=useState([]);
     const ENDPOINT='http://localhost:5000'
 
     useEffect(()=>{
@@ -15,26 +19,99 @@ const Chat=({location})=>{
         //console.log(location)
         //console.log(itNumber,roomId)
 
-        socket=io(ENDPOINT)
+        socket=io(ENDPOINT,{
+            transports:['websocket']
+        })
 
         setItNumber(itNumber)
         setRoomId(roomId)
         //console.log(socket)
-        socket.emit('join',{itNumber,roomId},({error})=>{
+        /*socket.emit('join',{itNumber,roomId},({error})=>{
             alert(error)
+        })*/
+        socket.emit('join',{itNumber,roomId},()=>{
+
         })
+
+        socket.on("connect_error",(err)=>{
+            console.log(`connect_error due to ${err.message}`);
+        })
+
+        //unmounting
+        return ()=>{
+            socket.disconnect()
+
+            socket.off()
+        }
 
     },[ENDPOINT,window.location.search])
 
+    //handle messages
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            //adding msg to msg array
+            setMessages((messages)=>[...messages,message])
+
+        })
+
+        socket.on("roomData",({itNumber})=>{
+            setItNumber(itNumber)
+        })
+
+    },[])
+
+    //func for sending messages
+    const sendMessage=(event)=>{
+        //console.log(event)
+        event.preventDefault()
+        if(message){
+            socket.emit('sendMessage',message,()=>setMessage(''))
+        }
+    }
+
+    console.log(message,messages)
+
     return(
-        <h1>Chat</h1>
+        <div>
+            
+            <div>
+                <ScrollToBottom>
+                    {messages.map((message,i)=>{
+                        return(
+                            <div key={i}>
+                                <div>
+                                    <div className="message-content">
+                                        <p>{message.text}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                        /*return(
+                            <div key={i}>
+                                <Message message={message} itNumber={itNumber}></Message>
+                            </div>
+                        )*/
+                        
+                        
+                    })}
+                </ScrollToBottom>
+            </div>
+            
+            <div>
+                <input
+                value={message}
+                onChange={(event)=>setMessage(event.target.value)}
+                onKeyPress={event=>event.key === 'Enter'?sendMessage(event):null}
+
+                />
+            </div>
+            <div>
+                <button onClick={(event)=>sendMessage(event)}>Send</button>
+            </div>
+        </div>
     )
 
 
 }
 
 export default Chat
-
-
-
-
